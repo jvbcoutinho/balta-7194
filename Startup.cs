f5c7fe474a3ccc2bbf01bb83cc31f97aa4b523ff
jvcoutinho <jv.coutinho@victorconsultoria.com.br>
2020-10-14 15:32:22 -0300

    Set httpsRedirection

--- Startup.cs
+++ Startup.cs
@@ -66,7 +66,7 @@ namespace Shop
             // if (env.IsDevelopment())
             app.UseDeveloperExceptionPage();
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
